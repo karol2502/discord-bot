e22741bd01dd37a6dfa7a1303cc926df2a9c01f4
Karol Angrys <karol.angrys@gmail.com>
2022-06-17 00:40:42 +0200

    Added show list and delete

---
+++ LegendaryBot/Modules/ReactionModule.cs
@@ -0,0 +1,81 @@
+﻿using Discord;
+using Discord.Commands;
+using LegendaryBot.Database;
+using LegendaryBot.Entities;
+
+namespace LegendaryBot.Modules
+{
+    public class ReactionModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly LegendaryDbContext _dbContext;
+
+        public ReactionModule(LegendaryDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [Command("ReactionAdd")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task ReactionAdd([Remainder] string messageAndReaction)
+        {
+            string[] text = messageAndReaction.Split(';');
+            string message = text[0].Trim();
+            string reaction = text[1].Trim();
+
+            var messageReactions = new MessageReaction()
+            {
+                Message = message.ToLower(),
+                Reaction = reaction,
+                GuildId = _dbContext.Guilds.FirstOrDefault(g => g.GuildId == Context.Guild.Id).Id,
+            };
+            await _dbContext.MessageReactions.AddAsync(messageReactions);
+            await _dbContext.SaveChangesAsync();
+
+            await ReplyAsync($"Message: {message}\nReaction: {reaction}\nAdded!");
+        }
+
+        [Command("ReactionList")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task ReactionList()
+        {
+            var id = _dbContext.Guilds.FirstOrDefault(g => g.GuildId == Context.Guild.Id).Id;
+
+            var messsageReactions = _dbContext.MessageReactions.Where(m => m.GuildId == id).ToList<MessageReaction>();
+
+            if (!messsageReactions.Any())
+            {
+                await ReplyAsync("Brak danych w bazie");
+                return;
+            }
+
+            string message = "";
+
+            foreach (var mr in messsageReactions)
+            {
+                message += $"Id: {mr.Id}\tMessage: {mr.Message}\tResponse: {mr.Reaction}\n";
+            }
+
+            await ReplyAsync(message);
+        }
+
+        [Command("ReactionDelete")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task ReactionDelete(int id)
+        {
+            var guildTableId = _dbContext.Guilds.FirstOrDefault(g => g.GuildId == Context.Guild.Id).Id;
+
+            var messageReaction = _dbContext.MessageReactions.FirstOrDefault(r => r.Id == id && r.GuildId == guildTableId);
+
+            if (messageReaction == null)
+            {
+                await ReplyAsync("Brak takiego rekordu");
+                return;
+            }
+
+            _dbContext.MessageReactions.Remove(messageReaction);
+            await _dbContext.SaveChangesAsync();
+
+            await ReplyAsync($"Usunięto reakcje");
+        }
+    }
+}
---
+++ LegendaryBot/Modules/ResponseModule.cs
@@ -0,0 +1,81 @@
+﻿using Discord;
+using Discord.Commands;
+using LegendaryBot.Database;
+using LegendaryBot.Entities;
+
+namespace LegendaryBot.Modules
+{
+    public class ResponseModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly LegendaryDbContext _dbContext;
+
+        public ResponseModule(LegendaryDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [Command("ResponseAdd")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task ResposneAdd([Remainder] string messageAndResponse)
+        {
+            string[] text = messageAndResponse.Split(';');
+            string message = text[0].Trim();
+            string response = text[1].Trim();
+
+            var messageResponse = new MessageResponse()
+            {
+                Message = message.ToLower(),
+                Response = response,
+                GuildId = _dbContext.Guilds.FirstOrDefault(g => g.GuildId == Context.Guild.Id).Id,
+            };
+            await _dbContext.MessageResponses.AddAsync(messageResponse);
+            await _dbContext.SaveChangesAsync();
+
+            await ReplyAsync($"Message: {message}\nResponse: {response}\nAdded!");
+        }
+
+        [Command("ResponseList")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task ResponseList()
+        {
+            var guildTableId = _dbContext.Guilds.FirstOrDefault(g => g.GuildId == Context.Guild.Id).Id;
+
+            var messageResponses = _dbContext.MessageResponses.Where(m => m.GuildId == guildTableId).ToList<MessageResponse>();
+
+            if (!messageResponses.Any())
+            {
+                await ReplyAsync("Brak danych w bazie");
+                return;
+            }
+
+            string message = "";
+
+            foreach (var mr in messageResponses)
+            {
+                message += $"Id: {mr.Id}\tMessage: {mr.Message}\tResponse: {mr.Response}\n";
+            }
+
+            await ReplyAsync(message);
+        }
+
+        [Command("ResponseDelete")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task ResponseDelete(int id)
+        {
+            var guildTableId = _dbContext.Guilds.FirstOrDefault(g => g.GuildId == Context.Guild.Id).Id;
+
+            var messageResponse = _dbContext.MessageResponses.FirstOrDefault(r => r.Id == id && r.GuildId == guildTableId);
+
+            if (messageResponse == null)
+            {
+                await ReplyAsync("Brak takiego rekordu");
+                return;
+            }
+
+            _dbContext.MessageResponses.Remove(messageResponse);
+            await _dbContext.SaveChangesAsync();
+
+            await ReplyAsync($"Usunięto odpowiedź");
+        }
+    }
+}
